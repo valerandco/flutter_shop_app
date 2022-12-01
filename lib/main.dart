2909cfc8dfa7fb7d27e3068f09f3e3377391e6e4
Valerii <vvv.gassiev@gmail.com>
2022-12-01 16:44:00 +0300

    edit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(
+      const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      )
+  );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.lightBlue,
+              ),
+            ),
+
+          ],
+
+        ),
+      ),
+    );
+  }
+}
